f5fb59c497785bc37e22bbd5865057ce5fdee100
dpdoescode <diegoperez9858985@outlook.com>
2025-09-27 15:50:21 -0400

    added more 5 more event cards

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -38,6 +38,41 @@ const Schedule = () => {
             description="Watch latest One Piece episode"
             image = "/images/Gear 5.jpg"/>
 
+            <Event event='Sleep' 
+            time='8:00 PM - 11:00 PM' 
+            color="gold" 
+            streak="6"
+            description="6-7 hours minimum"
+            image = "/images/Kemper.jpg"/>
+
+            <Event event='CS212' 
+            time='9:30 AM - 11:00 AM' 
+            color="grey" 
+            streak="14"
+            description="Try to pay attention."
+            image = "/images/Logic.jpg"/>
+
+            <Event event='EA3' 
+            time='11:00 AM - 11:50 AM' 
+            color="black" 
+            streak="8"
+            description="Lock in."
+            image = "/images/Electrical.jpg"/>
+
+            <Event event='AI' 
+            time='2:00 PM - 3:20 PM' 
+            color="purple" 
+            streak="12"
+            description="Can he stop yapping."
+            image = "/images/AI.jpg"/>
+
+            <Event event='Cognitive Science' 
+            time='3:30 PM - 5:00 PM' 
+            color="grey" 
+            streak="13"
+            description="Soothing Class!"
+            image = "/images/Primates.jpg"/>
+
         </div>
 
 
